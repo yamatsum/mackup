a7888fd968fa7267f8fa4fc712d293759dbd015c
yamatsum <sy12250@yahoo.co.jp>
2018-08-30 21:31:42 +0900

    install ndenv and tig

--- mackup.sh
+++ mackup.sh
@@ -4,3 +4,6 @@ ln -s ~/Library/Mobile\ Documents/com~apple~CloudDocs/mackup/.mackup.cfg ~/.mack
 
 # install tpm
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+# install node-build
+git clone https://github.com/riywo/node-build.git $(ndenv root)/plugins/node-build
