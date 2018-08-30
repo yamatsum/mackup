948d675008268fada75e013f6433284d14187d3d
yamatsum <sy12250@yahoo.co.jp>
2018-08-30 21:06:23 +0900

    Install tpm automatically

--- mackup.sh
+++ mackup.sh
@@ -1,3 +1,6 @@
 # for iCloud
 ln -s ~/Library/Mobile\ Documents/com~apple~CloudDocs/mackup/.mackup ~/.mackup
 ln -s ~/Library/Mobile\ Documents/com~apple~CloudDocs/mackup/.mackup.cfg ~/.mackup.cfg
+
+# install tpm
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
