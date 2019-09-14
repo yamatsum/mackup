944987486f819b0fdf1fb12f7c9878fdee175423
yamatsum <sy12250@gmail.com>
2019-09-15 00:56:44 +0900

    :sparkles: Add some alias.

--- .zsh/alias.zsh
+++ .zsh/alias.zsh
@@ -1,4 +1,6 @@
+alias v='nvim'
 alias vi='nvim'
+alias view='nvim -MR'
 alias ls='exa'
 alias la='exa --all'
 alias ll='exa --long'
@@ -11,4 +13,17 @@ alias tmuxn='tmux new -s'
 alias tmuxa='tmux a -t'
 alias tmuxl='tmux ls'
 alias st='stree'
+alias gh='github'
 # alias ssh='sshrc'
+alias d="docker"
+alias dps="d ps" #=docker container ls
+alias dpa="d ps -a"
+alias di="d image"
+alias dip="d image prune"
+alias dis="d images"
+alias db="d bulid"
+alias dex="d exec -i -t"
+alias dsp="d system prune"
+alias dl="d pull"
+alias k='kubectl'
+alias kc='kubectx'
