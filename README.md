5e9be2f06e7fd90860034e334f635808b4769c79
yamatsum <yamatsum@yahoo-corp.jp>
2020-10-16 23:13:02 +0900

    Updated Mac setup instructions.

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # mackup
+
+# Manual setup
+
+## Homebrew and Apps
+```
+brew install mas
+brew install rcmdnk/file/brew-file
+brew install mackup
+curl -sf https://raw.githubusercontent.com/yamatsum/mackup/master/setup.sh | sh -s
+mackup restore
+mas signin <apple ID>
+brew file install
+```
+
+## Setting
+Turn off Spotlight
+- [System Preferences-> keyboard -> Shortcut -> Spotlight]
+
+Show home in Sidebar of finder
+- [finder -> preferences -> Sidebar]
+
+Show calender in manu bar
+- [System Preferences -> date & Time -> Clock]
+ 
+Prefer tabs when opening documents to "always"
+- [System Preferences-> Dock]
+
--- setup.sh
+++ setup.sh
@@ -12,5 +12,71 @@ fi
 # setup zsh
 echo 'source "$XDG_CONFIG_HOME/zsh/zshrc"' >> ~/.zshrc
 
+# change default shell
+chsh -s /bin/zsh
+
 # install tpm
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+# change appearance dark
+defaults write "Apple Global Domain" "AppleInterfaceStyle" "Dark"
+
+# hide dock
+defaults write com.apple.dock autohide -bool true
+
+# remove recent app from dock
+defaults write com.apple.dock show-recents -bool FALSE
+
+# △ remove user name from menu bar
+# [System Preferences -> Users & Group -> Login Options]
+defaults write com.apple.systemuiserver menuExtras -array \
+    "/System/Library/CoreServices/Menu Extras/AirPort.menu" \
+    "/System/Library/CoreServices/Menu Extras/Battery.menu" \
+    "/System/Library/CoreServices/Menu Extras/Clock.menu"
+
+# display battery with percentage
+# [menubar -> battery]
+defaults write com.apple.menuextra.battery ShowPercent YES
+
+# change caps lock to control
+# [System Preferences-> keyboard -> modify key]
+keyboard_id="$(ioreg -c AppleEmbeddedKeyboard -r | grep -Eiw "VendorID|ProductID" | awk '{ print $4 }' | paste -s -d'-\n' -)-0"
+- defaults -currentHost write -g com.apple.keyboard.modifiermapping.${keyboard_id} -array-add "
+<dict>
+  <key>HIDKeyboardModifierMappingDst</key>\
+  <integer>30064771300</integer>\
+  <key>HIDKeyboardModifierMappingSrc</key>\
+  <integer>30064771129</integer>\
+</dict>"
+
+# change key repeat speed
+# [System Preferences-> keyboard]
+defaults write -g KeyRepeat -int 1
+defaults write -g InitialKeyRepeat -int 15
+
+# turn off capitalize words auto
+# [System Preferences-> Keyboard -> Text]
+defaults write NSGlobalDomain NSAutomaticCapitalizationEnabled -bool false
+
+# use three finger drag
+# [System Preferences-> accessibility -> Pointer Control -> Mouse & Trackpad]
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadThreeFingerDrag -bool true
+defaults write com.apple.AppleMultitouchTrackpad TrackpadThreeFingerDrag -bool true
+
+# △ tap with one finger
+# [System Preferences-> Trackpad]
+defaults write -g com.apple.mouse.tapBehavior -int 1
+
+# show all filename extensions
+# [finder -> preferences -> Advanced]
+defaults write -g AppleShowAllExtensions -bool true
+
+# show library in home
+# [Finder -> option + Go]
+chflags nohidden ~/Library && xattr -d com.apple.FinderInfo  ~/Library
+# show path bar in finder
+defaults write com.apple.finder ShowPathbar -bool true 
+
+# turn off auto rearrange spaces
+# [System Preferences-> Mission Control]
+defaults write com.apple.dock mru-spaces -bool false
