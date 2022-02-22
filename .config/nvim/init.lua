adf3426ea7cdc503225689c8668359092aba18f4
yamatsum <sy12250@gmail.com>
2022-02-23 00:36:50 +0900

    fix: set to map.

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,30 +1,29 @@
 local g = vim.g
 local o = vim.opt
 local cmd = vim.cmd
-local set = vim.keymap.set
+local map = vim.keymap.set
 
 o.number = true
 o.signcolumn = "yes"
-o.termguicolors = true
-o.shortmess = "IFc"
 o.expandtab = true
-o.showmode = false
 o.fillchars = { eob = " " }
-o.pumblend = 10
-o.winblend = 10
-o.ignorecase = true
-o.smartcase = true
+o.shortmess = "cFI"
 o.clipboard = "unnamedplus"
+o.ignorecase = true -- default
+o.smartcase = true -- default
 o.mouse = "a"
 o.shiftwidth = 2
 o.updatetime = 250
+o.termguicolors = true -- default
+o.showmode = false
+o.pumblend = 10 -- default
+o.winblend = 10 -- default
 
 g.mapleader = " "
 
-set({ "i", "v" }, "<C-j>", "<esc>", { silent = true })
-set("", "<Tab>", "gt", { silent = true })
-set("", "<S-Tab>", "gt", { silent = true })
-set("v", "*", 'y/\\V<C-R>"<CR>', { silent = true })
+map({ "i", "v" }, "<C-j>", "<esc>", { silent = true })
+map("", "<Tab>", "gt", { silent = true })
+map("", "<S-Tab>", "gT", { silent = true })
+map("v", "*", 'y/\\V<C-R>"<CR>', { silent = true })
 
 cmd("lan en_US.UTF-8")
-cmd("colorscheme one-nvim")
